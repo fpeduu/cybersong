5e01724d9c563cbd9957b79da645ff879c48e87a
karen <ksbs@cin.ufpe.br>
2025-03-27 13:14:39 -0300

    feat: add logo header, and songlist page

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SongList from "./pages/SongList";
+import Navbar from "./components/Navbar";
 
 function AppRoutes() {
+  const showNavbar = location.pathname !== "/";
   return (
     <Router>
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/song-list" element={<SongList />} />
       </Routes>
     </Router>
   );
